933a495c22639837e11778b58a4f5afdfc0b8b28
Dominik Stahl <dominik@samdj.de>
2026-05-25 18:12:34 +0200

    fix librewolf

--- modules/home/librewolf/default.nix
+++ modules/home/librewolf/default.nix
@@ -1,50 +1,48 @@
 { pkgs, lib, config, inputs, ... }:
 
 let
-  cfg = config.programs.librewolf;
-in
-lib.mkIf cfg.enable {
-  programs.librewolf.package = lib.mkForce null;
+  mkNixPak = inputs.nixpak.lib.nixpak {
+    inherit (pkgs) lib;
+    inherit pkgs;
+  };
 
-  home.packages = let
-    mkNixPak = inputs.nixpak.lib.nixpak {
-      inherit (pkgs) lib;
-      inherit pkgs;
-    };
+  sandboxed = mkNixPak {
+    config = { sloth, ... }: {
+      app.package = pkgs.librewolf;
+      flatpak.appId = "io.gitlab.librewolf";
 
-    sandboxed = mkNixPak {
-      config = { sloth, ... }: {
-        app.package = pkgs.librewolf;
-        flatpak.appId = "io.gitlab.librewolf";
+      bubblewrap = {
+        network = true;
 
-        bubblewrap = {
-          network = true;
+        bind.rw = [
+          (sloth.concat' sloth.homeDir "/.librewolf")
+          (sloth.env "XDG_RUNTIME_DIR")
+        ];
 
-          bind.rw = [
-            (sloth.concat' sloth.homeDir "/.librewolf")
-            (sloth.env "XDG_RUNTIME_DIR")
-          ];
+        bind.ro = [
+          "/run/current-system/sw/share"
+          "/tmp/.X11-unix"
+        ];
 
-          bind.ro = [
-            "/run/current-system/sw/share"
-            "/tmp/.X11-unix"
-          ];
+        bind.dev = [
+          "/dev/dri"
+          "/dev/shm"
+        ];
+      };
 
-          bind.dev = [
-            "/dev/dri"
-            "/dev/shm"
-          ];
-        };
-
-        dbus.enable = true;
-        dbus.policies = {
-          "org.freedesktop.DBus" = "talk";
-          "org.freedesktop.portal.*" = "talk";
-          "org.a11y.Bus" = "talk";
-        };
+      dbus.enable = true;
+      dbus.policies = {
+        "org.freedesktop.DBus" = "talk";
+        "org.freedesktop.portal.*" = "talk";
+        "org.a11y.Bus" = "talk";
       };
     };
-  in [
-    sandboxed.config.env
-  ];
+  };
+in {
+  programs.librewolf = {
+    enable = true;
+    package = lib.mkForce null;
+  };
+
+  home.packages = [ sandboxed.config.env ];
 }
